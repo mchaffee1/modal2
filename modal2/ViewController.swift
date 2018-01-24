1013d5c07353410706e1916d2e344bcc344c396b
Michael Chaffee <mchaffee@thoughtworks.com>
2018-01-24 13:57:47 -0500

    Initial Commit

---
+++ modal2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modal2
+//
+//  Created by Michael Chaffee on 1/24/18.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
